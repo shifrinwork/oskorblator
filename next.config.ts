441bacdcf167ef6fe01c075005078825a6a467dc
Sergey Shifrin <bernhard@MacBookPro.lan>
2026-05-22 01:55:02 +0300

    fix next.config and add eslint

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*.supabase.co",
-        pathname: "/storage/v1/object/public/**",
+        hostname: "**.supabase.co",
       },
     ],
   },
